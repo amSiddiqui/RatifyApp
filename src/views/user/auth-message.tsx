5e28bbebe3bb69638716656c09473d3d1f03c101
Aamir Mushtaq Siddiqui <33178990+TheKeySpammer@users.noreply.github.com>
2022-09-24 16:30:23 +0530

    auth message changes

--- src/views/user/auth-message.tsx
+++ src/views/user/auth-message.tsx
@@ -1,6 +1,5 @@
-import { Stack, Image, List } from '@mantine/core';
+import { Stack, Image } from '@mantine/core';
 import React from 'react';
-import { GoPrimitiveDot } from 'react-icons/go';
 
 const AuthMessage: React.FC = () => {
     return (
@@ -12,23 +11,19 @@ const AuthMessage: React.FC = () => {
             />
             <p className='lg:text-5xl text-4xl'>Digitalize document approvals and signing!</p>
             <p className='text-3xl'>Focus on growing your business.</p>
-            <List spacing={20} icon={<GoPrimitiveDot className='relative' style={{ top: 6 }} />}>
-                <List.Item>
-                    <p className="text-lg">
-                        Manual or via email approval &amp; signing of documents
-                        takes ages to complete, Leading to poor document management,
-                        as well as potentially damaging few trees in the process.
-                    </p>
-                </List.Item>
-                <List.Item>
-                    <p className='text-lg'>
-                        With Ratify this process takes seconds - literally for each
-                        user. Approvals, signing &amp; managing of contracts,
-                        agreement, non-disclosures is intuitive, efficient and
-                        environment friendly.
-                    </p>
-                </List.Item>
-            </List>
+            <Stack className='relative' style={{ top: 6 }} >
+                <p className="text-lg rounded-md bg-white p-4">
+                    Manual or via email approval &amp; signing of documents
+                    takes ages to complete, Leading to poor document management,
+                    as well as potentially damaging few trees in the process.
+                </p>
+                <p className='text-lg rounded-md p-4'>
+                    With Ratify this process takes seconds - literally for each
+                    user. Approvals, signing &amp; managing of contracts,
+                    agreement, non-disclosures is intuitive, efficient and
+                    environment friendly.
+                </p>
+            </Stack>
         </Stack>
     
     );
