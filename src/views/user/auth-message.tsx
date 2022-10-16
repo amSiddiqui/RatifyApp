7a3e57143b4a7f115664d3978ac579bf61998d18
Aamir Mushtaq Siddiqui <33178990+TheKeySpammer@users.noreply.github.com>
2022-10-16 23:43:22 +0530

    change bg color of auth message

--- src/views/user/auth-message.tsx
+++ src/views/user/auth-message.tsx
@@ -1,4 +1,4 @@
-import { Stack, Image } from '@mantine/core';
+import { Stack, Image, List } from '@mantine/core';
 import React from 'react';
 
 const AuthMessage: React.FC = () => {
@@ -12,17 +12,32 @@ const AuthMessage: React.FC = () => {
             <p className='lg:text-5xl text-4xl'>Digitalize document approvals and signing!</p>
             <p className='text-3xl'>Focus on growing your business.</p>
             <Stack className='relative' style={{ top: 6 }} >
-                <p className="text-lg rounded-md bg-white p-4">
-                    Manual or via email approval &amp; signing of documents
-                    takes ages to complete, Leading to poor document management,
-                    as well as potentially damaging few trees in the process.
-                </p>
-                <p className='text-lg rounded-md p-4'>
-                    With Ratify this process takes seconds - literally for each
-                    user. Approvals, signing &amp; managing of contracts,
-                    agreement, non-disclosures is intuitive, efficient and
-                    environment friendly.
-                </p>
+                <div className="text-lg rounded-md bg-rose-50 p-4 shadow-sm">
+                    <List icon={<span>&bull;</span>} spacing={8}>
+                        <List.Item>
+                            Manual or via email approval &amp; signing of documents
+                            takes ages to complete
+                        </List.Item>
+                        <List.Item>
+                            Leading to poor document management       
+                        </List.Item>
+                        <List.Item>
+                            As well as potentially damaging few trees in the process
+                        </List.Item>
+                    </List>
+                </div>
+                <div className='text-lg rounded-md bg-green-50 p-4 shadow-sm'>
+                    <List icon={<span>&bull;</span>} spacing={8}>
+                        <List.Item>
+                            With Ratify this process takes seconds - literally for each user
+                        </List.Item>
+                        <List.Item>
+                            Approvals, signing &amp; managing of contracts,
+                            agreement, non-disclosures is intuitive, efficient and
+                            environment friendly     
+                        </List.Item>
+                    </List>
+                </div>
             </Stack>
         </Stack>
     
